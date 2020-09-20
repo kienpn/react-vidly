39ff489b4e3fa50fb305b4457a6713733ef74832
unknown <phamkien.nuce@gmail.com>
2020-09-20 18:36:30 +0700

    done movies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 //import logo from './logo.svg';
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     // <React.Fragment>
     <main role="main" className="container">
-      <div className="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">
-          Use this document as a way to quickly start any new project.
-          <br /> All you get is this text and a mostly barebones HTML document.
-        </p>
-      </div>
+      <Movies />
     </main>
     // </React.Fragment>
   );
---
+++ src/components/movies.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    if (count === 0) {
+      return (
+        <span className="badge badge-warning">
+          There are no movies in the database.
+        </span>
+      );
+    }
+
+    return (
+      <div>
+        <span className="badge badge-primary">
+          Showing {count} movies in the database.
+        </span>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <th scope="row">{movie.title}</th>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
