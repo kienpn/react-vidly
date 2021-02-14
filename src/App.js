fb53c8fcb717803b8586c5ec7af3aa9946eaa159
unknown <phamkien.nuce@gmail.com>
2021-02-14 09:00:39 +0700

    done chapter 6 routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
-//import logo from './logo.svg';
-import "./App.css";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Customers from "./components/customers";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
+import "./App.css";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
-    // <React.Fragment>
-    <main role="main" className="container">
-      <Movies />
-    </main>
-    // </React.Fragment>
+    <React.Fragment>
+      <NavBar />
+      <main role="main" className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
